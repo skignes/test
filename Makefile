71536c6b7887b21401f0e8a8050552e3eea1a16e
Skignes <116216779+skignes@users.noreply.github.com>
2025-03-22 20:12:13 +0100

    feat(test): do something

---
+++ Makefile
@@ -0,0 +1,20 @@
+CC = gcc
+CFLAGS = -Wall -Wextra
+NAME = program
+SRC = main.c
+OBJ = $(SRC:.c=.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+    $(CC) $(CFLAGS) -o $(NAME) $(OBJ)
+
+clean:
+    rm -f $(OBJ)
+
+fclean: clean
+    rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    // Print all arguments if there are any
+    for (int i = 1; i < argc; i++) {
+        printf("%s\n", argv[i]);
+    }
+    
+    // Return 84 if more than 2 parameters (program name + 2 = 3 args total)
+    if (argc > 3) {
+        return 84;
+    }
+    
+    // Otherwise return 0
+    return 0;
+}
